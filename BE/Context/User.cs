a8541559228e0ce485dde0711dc73adf85ab6069
Cosmin Grigoriu <cosmin.grigoriu04@gmail.com>
2025-05-28 00:17:34 +0300

    remove comment

--- BE/Context/User.cs
+++ BE/Context/User.cs
@@ -5,5 +5,5 @@ public class User
 	public Guid Id { get; set; } = Guid.NewGuid();
 	public string Username { get; set; }
 	public string PasswordHash { get; set; }
-	public string Role { get; set; } // "Admin" or "User"
+	public string Role { get; set; }
 }
