f50182e3b8f5e32a64f4e08abeefe69117d44720
Cosmin Grigoriu <cosmin.grigoriu04@gmail.com>
2025-05-27 23:06:08 +0300

    use apiurl on fe

--- fe/src/services/AuthService.ts
+++ fe/src/services/AuthService.ts
@@ -4,13 +4,13 @@ const apiUrl = process.env.REACT_APP_API_URL;
 
 export class AuthService {
   static async login(username: string, password: string): Promise<string> {
-    const res = await axios.post(`/auth/login`, { username, password });
+    const res = await axios.post(`${apiUrl}/auth/login`, { username, password });
     const token = res.data.token;
     localStorage.setItem('jwt', token);
     return token;
   }
 
   static async register(username: string, password: string): Promise<void> {
-    await axios.post(`/auth/register`, { username, password });
+    await axios.post(`${apiUrl}/auth/register`, { username, password });
   }
 }
